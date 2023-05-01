6146e595d12172e3b2436e68a911429a27ca7831
lynnchepp <lynn286035@gmail.com>
2023-05-01 01:14:10 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
+ */
+int main(void)
+
+{
+	_putchar('p');
+	_putchar('_');
+	_putchar('u');
+	_putchar('h');
+	_putchar('t');
+	_putchar('c');
+	_putchar('r');
+	_putchar('a');
+	_putchar('\n');
+	return (0);
+}
