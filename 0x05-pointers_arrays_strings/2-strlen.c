d0ffdb7b550b7371e7760f0a2d6828a2a55f3419
lynnchepp <lynn286035@gmail.com>
2023-05-02 10:41:14 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - find the length of a string
+ * @s: string whose length is to be found
+ * Return: returns the length of @s
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s[count} != '\0)
+	count++;
+	return (count);
+}
