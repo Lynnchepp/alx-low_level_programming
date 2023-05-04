8be359619b31742f911ce7be8774237b2b280ed7
lynnchepp <lynn286035@gmail.com>
2023-05-04 02:38:55 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,7 +15,7 @@ char *cap_string(char *str)
 		if (str[index - 1] == ' ' ||
 				str[index - 1] == 't' ||
 				str[index - 1] == '\n' ||
-				srt[index - 1] == ',' ||
+				str[index - 1] == ',' ||
 				str[index - 1] == '.' ||
 				str[index - 1] == ';' ||
 				str[index - 1] == '(' ||
@@ -24,8 +24,8 @@ char *cap_string(char *str)
 				str[index - 1] == '}' ||
 				str[index - 1] == '!' ||
 				str[index - 1] == '?' ||
-				srt[index - 1] == '"' ||
-				index == o)
+				str[index - 1] == '"' ||
+				index == 0)
 			str[index] -= 32;
 		index++;
 	}
