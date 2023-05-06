a00ff0ee28bc2f5fba361edc7c56e741bf76be51
lynnchepp <lynn286035@gmail.com>
2023-05-05 23:07:43 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,33 +1,39 @@
 #include "main.h"
+#include <stddef.h>
 /**
- * cap_string - capitalizes all words of a string
- * @str: input string
- * Return: pointer to the modified string
+ * cap_string - capitalizes all words in a string
+ * @str: pointer to input string
+ *
+ * Return: pointer to modified string
  */
 char *cap_string(char *str)
 {
-	int index = 0;
+	int i;
+	int cap_next = 1;
 
-	while (str[index])
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-		if (str[index - 1] == ' ' ||
-				str[index - 1] == 't' ||
-				str[index - 1] == '\n' ||
-				str[index - 1] == ',' ||
-				str[index - 1] == '.' ||
-				str[index - 1] == ';' ||
-				str[index - 1] == '(' ||
-				str[index - 1] == ')' ||
-				str[index - 1] == '{' ||
-				str[index - 1] == '}' ||
-				str[index - 1] == '!' ||
-				str[index - 1] == '?' ||
-				str[index - 1] == '"' ||
-				index == 0)
-			str[index] -= 32;
-		index++;
+		if (cap_next && str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 'a' + 'A';
+		}
+		cap_next = 0;
+		if (str[i] == ' ' ||
+				str[i] == '\t' ||
+				str[i] == '\n' ||
+				str[i] == ',' ||
+				str[i] == ';' ||
+				str[i] == '.' ||
+				str[i] == '!' ||
+				str[i] == '?' ||
+				str[i] == '"' ||
+				str[i] == '(' ||
+				str[i] == ')' ||
+				str[i] == '{' ||
+				str[i] == '}')
+		{
+			cap_next = 1;
+		}
 	}
 	return (str);
 }
